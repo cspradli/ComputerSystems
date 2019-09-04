2a44f7e6c3cf10d0d36cec25bfcf43f038487709
cspradli <cspradli@unca.edu>
2019-09-03 20:20:30 -0400

    updated .h with all functions to be implemented

--- linked_list/linked_list.h
+++ linked_list/linked_list.h
@@ -3,6 +3,9 @@
  * For Systems II
  */
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 /*
  * This program implements a linked list
  */
@@ -30,8 +33,36 @@ linked_list *create_linked_list();
 /*
  * Destroy linked list
  *
- * Returns true if succesfulle
+ * Returns true if succesfull
  * Returns false if ll is Null or otherwise unsuccesfull
  *
  */
 bool linked_list_destroy(linked_list *ll);
+
+/*
+ *
+ * Add to linked list
+ * Returns true if succesfull
+ * Returns false if ll is null or otherwise unsuccesfull
+ */
+bool linked_list_add(linked_list *ll, char* key);
+
+/*
+ * Delete element from linked list
+ * Returns true if succesfull
+ * Returns false if ll is null, empty, or otherwise unsuccesfull
+ */
+bool linked_list_delete(linked_list *ll, char* key);
+
+/*
+ * Search for element by key
+ * Returns node with correct key
+ */
+node linked_list_search(linked_list *ll, char* key);
+
+/*
+ * Gets the node based on position
+ * Returns the node
+ * Reuturns 0 if unsuccesful
+ */
+node linked_list_get(linked_list *ll, int position);
