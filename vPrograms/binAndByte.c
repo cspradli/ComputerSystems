8be7de03ad532cb3b3d2887f7d0358ccdc6cbe34
cspradli <cspradli@unca.edu>
2019-09-08 13:34:11 -0400

    added third test case

--- vPrograms/binAndByte.c
+++ vPrograms/binAndByte.c
@@ -1,4 +1,5 @@
 /*
+ *
  * Program to run some conversions and outputs for various homeworks and labs
  * Author: Caleb Spradlin
  * Class: CS 335- Systems II
@@ -15,9 +16,13 @@ int main(int argc, char const *argv[])
     printf("1a       a=%u\n", a);
     printf("1b       a=%u\n", a << 2);
     
+    /* Should return some sort of overflow */
     int b = 0xFFFF000c;
     printf("2a       b=%u\n", b);
     printf("2b       b=%u\n", b << 15);
 
+    int c = 0xFFFF000c;
+    printf("3a       c=%i\n", c);
+    printf("3b       c=%i\n", c >> 16);
     return 0;
 }
