350390723609e91538e6eaa539d306b062e25569
Caleb Spradlin <cspradli@montreat.cs.unca.edu>
2019-09-15 15:32:19 -0400

    added dir_print dec and some code

--- directory_ll/directory.c
+++ directory_ll/directory.c
@@ -4,3 +4,20 @@
  * Author: Caleb Spradlin
  * Date: 09/15/19
  */
+#include <dirent.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int dir_print(char *);
+
+int dir_print(char *path_name){
+    char largeFileBuffer[8192];
+    DIR *dirPointer = opendir(path_name);;
+    if (dirPointer != NULL){
+        
+    }
+}
--- directory_ll/main.c
+++ directory_ll/main.c
@@ -1,3 +1,10 @@
+/*
+ * Main program for directory printing
+ * Goes in conjunction with directory and linked_list files
+ * Author: Caleb Spradlin
+ * Date: 09/15/19
+ */
+
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
