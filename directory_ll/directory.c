90ba19d31e552df7663451904ffe5c2d4b01f74d
Caleb Spradlin <cspradli@montreat.cs.unca.edu>
2019-09-15 17:04:32 -0400

    succesfull make

--- directory_ll/directory.c
+++ directory_ll/directory.c
@@ -4,21 +4,37 @@
  * Author: Caleb Spradlin
  * Date: 09/15/19
  */
-#include <dirent.h>
+
+#include <dirent.h> 
+#include <sys/types.h> 
+#include <sys/param.h> 
+#include <sys/stat.h> 
+#include <unistd.h> 
+#include <stdio.h> 
+#include <string.h> 
 #include <errno.h>
-#include <string.h>
-#include <unistd.h>
 #include "directory.h"
 
 #define MAX_PATH 1024
 
 void dir_put(char *path_name){
     linked_list *ll;
+    struct dirent *dir;
     char name[MAX_PATH];
-    Dirent *dp;
     DIR *dirPointer;
 
     ll = create_linked_list();
+
+    if ((dirPointer = opendir(path_name)) == NULL){
+        fprintf(stderr, "Can't open %s\n", path_name);
+        return;
+    }
+    while ((dir = readdir(dirPointer)) != NULL){
+        if (strcmp(dir->d_name, ".") == 0 || strcmp(dir->d_name, "..") == 0) continue;
+        sprintf(name, "%s/%s", path_name, dir->d_name);
+        linked_list_add(ll, dir->d_name);
+    }
+    closedir(dirPointer);
 }
 
 void dir_print(linked_list *ll){
