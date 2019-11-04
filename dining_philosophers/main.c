ba0416f89286c30d28027d94d8e0e1143bd42ae5
cspradli <cspradli@unca.edu>
2019-11-04 15:24:03 -0500

    Added dining philos

---
+++ dining_philosophers/main.c
@@ -0,0 +1,26 @@
+#include "philosopher.h"
+#include "semaphore_lib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+int main(int argc, char**argv){
+
+    semaphore_init(); // initialize semaphorea
+    create_5_philosophers();
+
+    int wait_time = 60;
+    if(argc > 1){
+        wait_time = atoi(argv[1]);
+    }
+    printf("PARENT: sleeping for %i seconds\n", wait_time);
+    sleep(wait_time);
+
+    kill_5_philosophers();
+    sleep(2);  // wait for the childen to die
+    semaphore_destroy(); // cleanup all semaphores
+    return 0;
+}
+
---
+++ dining_philosophers/philosopher.h
@@ -0,0 +1,12 @@
+#ifndef philosopher_h
+#define philosopher_h
+
+
+void create_5_philosophers();
+
+void kill_5_philosophers();
+
+void philosopher_algorithm(int num);
+
+
+#endif //philosopher_h
